11e6c809535137af00fdfcf5eb254d4ae06dabe0
Davide Di Criscito <dcdavidev@gmail.com>
2026-03-13 14:22:36 +0100

    feat(@pittorica/website): move carousel into component gallery section

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -125,119 +125,6 @@ export default function Route() {
         </Container>
       </Section>
 
-      <Section p="9">
-        <Container maxWidth="lg">
-          <Flex direction="column" gap="8">
-            <Flex direction="column" gap="2">
-              <Heading size="8" weight="bold">
-                Visual Showcase
-              </Heading>
-              <Heading
-                as="p"
-                size="4"
-                weight="regular"
-                style={{ opacity: 0.6 }}
-              >
-                Experience the painterly aesthetic through our curated gallery.
-              </Heading>
-            </Flex>
-
-            <Carousel.Root defaultIndex={0}>
-              <Carousel.Item
-                src="/static/gallery/jc-gellidon-vP3ORiYUBTY-unsplash.jpg"
-                alt="Gallery Image 1"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Abstract Forms
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/john-zhou-o_u0XTbvrPc-unsplash.jpg"
-                alt="Gallery Image 2"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Natural Textures
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/julien-BAR9_WJTcNI-unsplash.jpg"
-                alt="Gallery Image 3"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Urban Perspective
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/land-o-lakes-inc-1w3tO5F8HYY-unsplash.jpg"
-                alt="Gallery Image 4"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Rural Serenity
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/mitchell-orr-7eFWNqPToxk-unsplash.jpg"
-                alt="Gallery Image 5"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Geometric Harmony
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/nir-himi-w_rVpVZGa3g-unsplash.jpg"
-                alt="Gallery Image 6"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Ethereal Glow
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/patrick-untersee-j3f1lwXBuAI-unsplash.jpg"
-                alt="Gallery Image 7"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Mountain Majesty
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/petar-avramoski-YKTIzSSd4ug-unsplash.jpg"
-                alt="Gallery Image 8"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Architectural Depth
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-              <Carousel.Item
-                src="/static/gallery/willian-justen-de-vasconcellos-jxnHGSSQENo-unsplash.jpg"
-                alt="Gallery Image 9"
-              >
-                <Carousel.Description>
-                  <Heading size="3" weight="bold">
-                    Dynamic Motion
-                  </Heading>
-                </Carousel.Description>
-              </Carousel.Item>
-            </Carousel.Root>
-          </Flex>
-        </Container>
-      </Section>
-
       <Section p="9" style={{ backgroundColor: 'var(--pittorica-surface-1)' }}>
         <Container maxWidth="lg">
           <Flex direction="column" gap="8">
@@ -251,8 +138,8 @@ export default function Route() {
                 weight="regular"
                 style={{ opacity: 0.6 }}
               >
-                A collection of elegant, painterly components for your next
-                project.
+                A collection of elegant, painterly components and visual
+                examples for your next project.
               </Heading>
             </Flex>
 
@@ -418,6 +305,99 @@ export default function Route() {
                 </Flex>
               </Card>
             </Grid>
+
+            <Carousel.Root defaultIndex={0}>
+              <Carousel.Item
+                src="/static/gallery/jc-gellidon-vP3ORiYUBTY-unsplash.jpg"
+                alt="Gallery Image 1"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Abstract Forms
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/john-zhou-o_u0XTbvrPc-unsplash.jpg"
+                alt="Gallery Image 2"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Natural Textures
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/julien-BAR9_WJTcNI-unsplash.jpg"
+                alt="Gallery Image 3"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Urban Perspective
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/land-o-lakes-inc-1w3tO5F8HYY-unsplash.jpg"
+                alt="Gallery Image 4"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Rural Serenity
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/mitchell-orr-7eFWNqPToxk-unsplash.jpg"
+                alt="Gallery Image 5"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Geometric Harmony
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/nir-himi-w_rVpVZGa3g-unsplash.jpg"
+                alt="Gallery Image 6"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Ethereal Glow
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/patrick-untersee-j3f1lwXBuAI-unsplash.jpg"
+                alt="Gallery Image 7"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Mountain Majesty
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/petar-avramoski-YKTIzSSd4ug-unsplash.jpg"
+                alt="Gallery Image 8"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Architectural Depth
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+              <Carousel.Item
+                src="/static/gallery/willian-justen-de-vasconcellos-jxnHGSSQENo-unsplash.jpg"
+                alt="Gallery Image 9"
+              >
+                <Carousel.Description>
+                  <Heading size="3" weight="bold">
+                    Dynamic Motion
+                  </Heading>
+                </Carousel.Description>
+              </Carousel.Item>
+            </Carousel.Root>
           </Flex>
         </Container>
       </Section>
